a22cacae80f5db18a21857f7bc27a54c3a5d003a
Marcelo Queiroz de Santana <marcelqdstrab@gmail.com>
2022-08-05 17:44:16 -0300

    base project create

---
+++ src/main/java/com/mqds/cloudpark/CloudParkApplication.java
@@ -0,0 +1,13 @@
+package com.mqds.cloudpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudParkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudParkApplication.class, args);
+	}
+
+}
